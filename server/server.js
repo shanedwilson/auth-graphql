c2e887c1ac1098acf55ac3e5d70497bb0413b145
Shane D Wilson <shane.wilson@getbuilt.com>
2024-01-07 16:15:40 -0600

    chapter 82 complete

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your Mongo Atlas URI
-const MONGO_URI = '';
+const MONGO_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.doepbt8.mongodb.net/auth-graphql?retryWrites=true&w=majority`
 if (!MONGO_URI) {
   throw new Error('You must provide a Mongo Atlas URI');
 }
