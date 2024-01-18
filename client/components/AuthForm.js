a4ee32ee07170f4d63970b6adc1952cdb398b512
Shane D Wilson <shane.wilson@getbuilt.com>
2024-01-18 09:12:20 -0600

    chapter 103 complete

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -33,6 +33,11 @@ class AuthForm extends Component {
                             
                         />
                     </div>
+
+                    <div className='errors'>
+                        {this.props.errors.map(error => <div key={error}>{error}</div>)}
+                    </div>
+
                     <button className='btn'>Submit</button>
                 </form>
             </div>
