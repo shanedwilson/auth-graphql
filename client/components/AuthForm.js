0485b699269ff322820258fefa96c40ce36b8f75
Shane D Wilson <shane.wilson@getbuilt.com>
2024-01-18 08:49:14 -0600

    chapter 100 complete

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -7,20 +7,27 @@ class AuthForm extends Component {
         this.state = { email: '', password: '' }
     }
 
+    onSubmit(event) {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state);
+    }
+
     render() {
         return (
             <div className='row'>
-                <form className='col s4'>
+                <form onSubmit={this.onSubmit.bind(this)} className='col s4'>
                     <div className='input-field'>
-                        <label htmlFor="">Email</label>
                         <input
+                            placeholder='Email'
                             value={this.state.email}
                             onChange={e => this.setState({ email: e.target.value })}
                         />
                     </div>
                     <div className='input-field'>
-                        <label htmlFor="">Password</label>
                         <input
+                            placeholder='Password'
+                            type='password'
                             value={this.state.password}
                             onChange={e => this.setState({ password: e.target.value })}
                             
