421763db31abcc89a4aea996d3c8597ec29fe907
Shane Wilson <40958758+shanedwilson@users.noreply.github.com>
2024-01-22 17:14:04 -0600

    Section14 (#3)
    * chapter 105 complete
    
    * chapter 111 complete
    
    * Chapter 113 complete
    
    * chapter 114 complete
    
    * chapter 115 complete

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import AuthForm from './AuthForm';
 import { graphql } from 'react-apollo'
-import mutation from '../mutations/Signup'
+import mutation from '../mutations/Signup';
+import query from '../queries/CurrentUser';
+import { hashHistory } from 'react-router';
 
 class SignupForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { errors: [] };
+    }
+
+    componentWillUpdate(nextProps) {
+        // this.props old/current props
+        // nextProps new/next props
+
+        if (!this.props.data.user && nextProps.data.user) {
+            hashHistory.push('/dashboard');
+        }
+    }
+
     onSubmit({ email, password }) {
         this.props.mutate({
-            variables: { email, password }
-        })
+            variables: { email, password },
+            refetchQueries: [{ query }]
+        }).catch(res => {
+            const errors = res.graphQLErrors.map(error => error.message);
+            this.setState({ errors });
+        });
     }
 
     render() {
@@ -15,7 +36,7 @@ class SignupForm extends Component {
             <div>
                 <h3>Sign Up</h3>
                 <AuthForm
-                    errors={[]}
+                    errors={this.state.errors}
                     onSubmit={this.onSubmit.bind(this)}
                 />
             </div>
@@ -23,4 +44,6 @@ class SignupForm extends Component {
     }
 }
 
-export default graphql(mutation)(SignupForm);
+export default graphql(query)(
+    graphql(mutation)(SignupForm)
+);
